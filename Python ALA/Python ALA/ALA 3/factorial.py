99098ef56c3f4c71eec4d69049d2a0a032306233
Ayushii1909 <maltisolanki920@gmail.com>
2026-04-07 18:55:33 +0530

    Add files via upload

---
+++ Python ALA/Python ALA/ALA 3/factorial.py
@@ -0,0 +1,9 @@
+# Factorial program in Python
+
+n = 5
+fact = 1
+
+for i in range(1, n + 1):
+    fact *= i
+
+print("Factorial =", fact)
---
+++ Python ALA/Python ALA/ALA 3/main.java
@@ -0,0 +1,11 @@
+public class main {
+ public static void main(String[] args) {
+ int n=5;
+//Sr. Enrollment No. Lang Topic Code (Convert to Python)
+ long fact=1;
+ for(int i=1; i<=n; i++) {
+ fact *= i;
+ }
+ System.out.println("Factorial = " + fact);
+ }
+}
